c4718b11b4ce50f9ccdae4d5bbad38e506e166c9
akash <akash300383@gmail.com>
2022-09-18 12:38:50 +0530

    done

---
+++ Programs/JavaScript Using ES6/user-defined-object.js
@@ -0,0 +1,13 @@
+// literal style object creation with only property 
+let emp1 = {id:100,name:"Ravi",age:21};
+let emp2 = {id:101,name:"Ajay",age:25};
+document.write("<br/> Literal style object")
+document.write("<br/> First employee details");
+document.write("<br/> id "+emp1.id);
+document.write("<br/> name "+emp1.name);
+document.write("<br/> age "+emp1.age);
+document.write("<br/> Second employee details");
+document.write("<br/> id "+emp2.id);
+document.write("<br/> name "+emp2.name);
+document.write("<br/> age "+emp2.age);
+
