e82f3a4a5ff05c09f0539168163e9ec86d68e41b
akash <akash300383@gmail.com>
2022-09-24 12:34:58 +0530

    done

---
+++ Programs/JavaScript Using ES6/asynoperation.js
@@ -0,0 +1,25 @@
+document.write("Hi<br/>")
+    let i=0;
+    let j=0;
+    setTimeout(()=> {
+        //document.write("Hello<br/>");
+        document.getElementById("d1").innerHTML="Hello"
+    },3000) 
+    setInterval(()=> {
+        document.getElementById("d2").innerHTML="I Am fine "+i;
+        i++;
+    },1000);
+
+
+ 
+    setInterval(()=> {
+        if(j%2==0){
+            document.getElementById("d3").style.color="RED"
+            document.getElementById("d3").setAttribute("","");
+        }else {
+            document.getElementById("d3").style.color="GREEN"
+        }
+        
+        j++;
+    },1000);
+document.write("How r you<br/>")
